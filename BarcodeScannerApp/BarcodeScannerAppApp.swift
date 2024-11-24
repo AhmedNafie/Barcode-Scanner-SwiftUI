1f91e6bdd43ba7c3bd6940e68c98659ac2ac19bf
Ahmed Nafie <101005449+AhmedNafie@users.noreply.github.com>
2024-11-24 10:59:57 +0200

    Initial Commit

---
+++ BarcodeScannerApp/BarcodeScannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerAppApp.swift
+//  BarcodeScannerApp
+//
+//  Created by Didia on 24/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
