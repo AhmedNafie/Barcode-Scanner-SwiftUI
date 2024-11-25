7d972500c9d9d77375f542f0d3a7927d703b3261
Ahmed Nafie <101005449+AhmedNafie@users.noreply.github.com>
2024-11-25 11:55:43 +0200

    Create ScannerViewController

---
+++ BarcodeScannerApp/ScannerViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ScannerViewController.swift
+//  BarcodeScannerApp
+//
+//  Created by Ahmed Nafie on 25/11/2024.
+//
+
+import UIKit
+import AVFoundation
+
+final class ScannerViewController: UIViewController {
+    
+}
